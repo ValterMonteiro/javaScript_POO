32ac261410357c24fa31d68cb623a5dc939098d2
ValterMonteiro <valtinms@gmail.com>
2023-02-13 17:10:18 -0400

    💄 add or update the ui  and style files

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente{
     agencia;
     saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Valter";
 cliente1.cpf = 11122233301;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 11122233302;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const contaCorrenteValter = new ContaCorrente();
+contaCorrenteValter.saldo = 0;
+contaCorrenteValter.agencia = 1001;
+
+console.log(contaCorrenteValter.saldo);
+contaCorrenteValter.saldo = 100;
+console.log(contaCorrenteValter.saldo);
+
+contaCorrenteValter.sacar(200); //se o valor for maior, não executa
+console.log(contaCorrenteValter.saldo);
+
+
+console.log(cliente1);
+console.log(cliente2);
